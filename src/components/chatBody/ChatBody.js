ed989688b9b8f073d28ba4adfe87e67270e64be9
sai srinivas vitesh <nviteshn@gmail.com>
2022-10-14 19:32:23 +0530

    v4

--- src/components/chatBody/ChatBody.js
+++ src/components/chatBody/ChatBody.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import "./chatBody.css";
 import ChatList from "../chatList/ChatList";
-//import ChatContent from "../chatContent/ChatContent";
-//import UserProfile from "../userProfile/UserProfile";
+
 
 export default class ChatBody extends Component {
   render() {
